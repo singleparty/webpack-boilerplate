871f6a1f6c06f28ff86d6ae8e63dc2cdd626b415
yiyu <singleparty2012@gmail.com>
2023-03-16 02:14:43 +0800

    feat: 支持 ?.

--- babel.config.js
+++ babel.config.js
@@ -1,6 +1,4 @@
 module.exports = {
-  presets: [
-    '@vue/cli-plugin-babel/preset', '@babel/preset-typescript'
-  ],
-  "plugins": ["@babel/plugin-proposal-class-properties"]
+  presets: ['@vue/cli-plugin-babel/preset', '@babel/preset-typescript'],
+  plugins: ['@babel/plugin-proposal-class-properties', '@babel/plugin-proposal-optional-chaining']
 }
