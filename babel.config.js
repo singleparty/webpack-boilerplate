a0de7e22b1bd9ce1841eff7a501e046e5c53b69d
yiyu <yiyu@pbcdci.cn>
2023-04-07 09:36:29 +0800

    基础的

--- babel.config.js
+++ babel.config.js
@@ -1,4 +1,4 @@
 module.exports = {
   presets: ['@vue/cli-plugin-babel/preset', '@babel/preset-typescript'],
-  plugins: ['@babel/plugin-proposal-class-properties', '@babel/plugin-proposal-optional-chaining']
+  plugins: ['@babel/plugin-proposal-class-properties']
 }
